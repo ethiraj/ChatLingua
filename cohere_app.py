0018ab67f83e36a1dda3bb45be9f35c476e8a6bf
ethiraj <ethiraj>
2023-04-02 23:52:44 -0500

    updated to streamlit app

--- cohere_app.py
+++ cohere_app.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template, request
+import streamlit as st
 from langchain.llms import Cohere
 
-app = Flask(__name__)
 cohere = Cohere(model='command-xlarge')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+st.set_page_config(page_title="LinguaBot App", page_icon=":guardsman:", layout="wide")
 
-@app.route('/generative-ai', methods=['POST'])
-def predict():
-    text = request.form['text']
-    response = cohere(text)
-    return render_template('index.html', text=text, response=response)
+st.markdown(
+    """
+    <style>
+    .navbar {
+        background-color: orange;
+    }
+    .navbar-brand {
+        color: white;
+        font-size: 30px;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+menu = ["Home"]
+choice = st.sidebar.selectbox("Select an option", menu)
+
+if choice == "Home":
+    st.title("LinguaBot App")
+    st.header("A fun and interactive way to explore the world of natural language processing with support for a variety of LLMs")
+    text = st.text_input("Enter a text:")
+    if st.button("Submit"):
+        response = cohere(text)
+        st.write("You entered:")
+        st.write(text)
+        st.write("Response:")
+        st.write(response)
